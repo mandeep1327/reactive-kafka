b66c62511f80d2ae7bcd0bae381e6fd3a2fa3457
shekharpatil <chandrashekhar.patil@maersk.com>
2021-06-25 14:02:48 +0200

    added dummy tests

---
+++ src/test/java/net/apmoller/crb/microservices/external/apis/dcsa/processor/DcsaEventProcessorApplicationTest.java
@@ -0,0 +1,11 @@
+package net.apmoller.crb.microservices.external.apis.dcsa.processor;
+
+
+import org.junit.jupiter.api.Test;
+
+class DcsaEventProcessorApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
